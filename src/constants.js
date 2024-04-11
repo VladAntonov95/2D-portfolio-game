b6f86aed237d4be610da42c0c8c144d4ca771d61
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-04-11 15:07:51 +0300

    fix/ fix computer bug

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/React/Next.js/TypeScript these days.
-      I've made a couple of projectsin that language. I also use Node.js/Express and Nest.js. Anyway regardless of the language, I just like programming.
+  pc: `This is my PC. I work mostly in JavaScript, React, Next.js, TypeScript these days.
+      I've made a couple of projectsin that language. I also use Node.js, Express and Nest.js. Anyway regardless of the language, I just like programming.
       Here is my <a href="https://github.com/VladAntonov95" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Netflix. 
