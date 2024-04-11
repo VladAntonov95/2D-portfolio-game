730b942f6801b1319475fe9d4a663cc3a9ce2030
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-04-11 15:49:33 +0300

    feat/add_resume

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const dialogueData = {
     You'll learn how I built this portfolio you're currently playing through!`,
   tv: `That's my TV. I've been watching tech youtubers a lot.`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. Contact me if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my <a href="./resume.pdf" target="_blank">resume</a>. Contact me if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
   library: `There are a lot of programming books on my shelves.
